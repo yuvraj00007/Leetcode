5506290274df0726f176148641c8f9ce91d7346c
Yuvraj Singh Chauhan <neelamsinghchauhan778@gmail.com>
2026-06-26 01:25:03 +0530

    Time: 27 ms (24.26%), Space: 21.1 MB (27.45%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -8,7 +8,9 @@ class Solution:
                 ans.append(nums[i])
                 break
 
+        num_set=set(nums)
+
         for i in range(1,n+1):
-            if i not in nums:
+            if i not in num_set:
                 ans.append(i)
         return ans
