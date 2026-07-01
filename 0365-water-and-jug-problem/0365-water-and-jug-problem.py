bcca725863d45cc1ee272224e5083cb900fe3afc
Yuvraj Singh Chauhan <neelamsinghchauhan778@gmail.com>
2026-07-01 23:43:18 +0530

    Time: 0 ms (100%), Space: 19.3 MB (45.22%) - LeetHub

--- 0365-water-and-jug-problem/0365-water-and-jug-problem.py
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.py
@@ -2,13 +2,13 @@ class Solution:
     def canMeasureWater(self, x: int, y: int, target: int) -> bool:
         def gcd(a,b):
             if b==0:
-                return a
+                return a 
             return gcd(b,a%b)
-                
-            
+        
         z=target
+
         if x+y<z:
             return False
-        if x==z or y==z or x+y==z:
+        if x==z or y==z or x+y ==z:
             return True
         return z%gcd(x,y)==0
