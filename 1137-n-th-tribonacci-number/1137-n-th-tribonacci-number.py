dd07264f975206166daf9eb651563d816c09ce63
Yuvraj Singh Chauhan <neelamsinghchauhan778@gmail.com>
2026-07-12 15:34:45 +0530

    Time: 0 ms (100%), Space: 19.2 MB (53.64%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -1,19 +1,15 @@
+from typing import List
+
 class Solution:
     def tribonacci(self, n: int) -> int:
-        f=0
-        s=1
-        t=1
+        if n == 0:
+            return 0
+        if n <= 2:
+            return 1
 
-        c=n-2
-        if n==0:return 0
-        if n==1:return 1
-        if n==2:return 1
-        if n==3:return 2
-        
-        while c:
-            summ=(f+s+t)
-            f=s
-            s=t
-            t=summ
-            c-=1
-        return summ
+        f, s, t = 0, 1, 1
+
+        for _ in range(n - 2):
+            f, s, t = s, t, f + s + t
+
+        return t
