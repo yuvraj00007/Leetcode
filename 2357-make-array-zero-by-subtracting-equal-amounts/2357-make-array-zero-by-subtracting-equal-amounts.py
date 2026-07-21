134550390298922f50d92e62204e121f069f6c1e
Yuvraj Singh Chauhan <neelamsinghchauhan778@gmail.com>
2026-07-21 19:31:55 +0530

    Time: 13 ms (5.21%), Space: 19.5 MB (17.63%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
@@ -1,3 +1,19 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
-        return len(set(nums) - {0})
+        heapq.heapify(nums)
+        ans=0
+        while nums:
+            x=heapq.heappop(nums)
+            if x==0:
+                continue
+            ans+=1
+
+            tmp=[]
+            while nums:
+                val=heapq.heappop(nums)
+                if val!=0:
+                    tmp.append(val-x)
+            for val in tmp:
+                heapq.heappush(nums,val)
+            
+        return ans
