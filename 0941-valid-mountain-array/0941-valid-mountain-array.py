38d68d18f0ce8a8c58be877b2b554ea22e989fcc
Yuvraj Singh Chauhan <neelamsinghchauhan778@gmail.com>
2026-07-29 23:16:20 +0530

    Time: 153 ms (86.68%), Space: 20.4 MB (78.77%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.py
+++ 0941-valid-mountain-array/0941-valid-mountain-array.py
@@ -1,22 +1,15 @@
 class Solution:
     def validMountainArray(self, arr: List[int]) -> bool:
-        if len(arr)<3:
-            return False
-        dec=0
-        only_inc=0
-        only_dec=0
-        for i in range(1,len(arr)):
-            if arr[i]==arr[i-1]:
-                return False
-            if arr[i]>arr[i-1]:
-                if dec==1:
-                    return False
-                only_dec=1
+        i=0
+        n=len(arr)
 
-
-            if arr[i]<arr[i-1]:
-                dec=1
-                only_inc=1
-        if only_inc==0 or only_dec==0:
+        while i+1<n and arr[i]<arr[i+1]:
+            i+=1
+        
+        if i==0 or i==n-1:
             return False
-        return True
+        
+        while i+1<n and arr[i]>arr[i+1]:
+            i+=1
+        
+        return i==n-1
