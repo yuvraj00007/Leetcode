b0462a7e7fe15c8da24afdbdbbebb60c8abfc6c1
Yuvraj Singh Chauhan <neelamsinghchauhan778@gmail.com>
2026-08-01 12:19:47 +0530

    Time: 3 ms (21.45%), Space: 19.3 MB (43.25%) - LeetHub

---
+++ 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findSubarrays(self, nums: List[int]) -> bool:
+        window=sum(nums[:2])
+        ans=[]
+        ans.append(window)
+        for i in range(2,len(nums)):
+            window+=nums[i]
+            window-=nums[i-2]
+            if window in ans:
+                return True
+            ans.append(window)
+        return False
