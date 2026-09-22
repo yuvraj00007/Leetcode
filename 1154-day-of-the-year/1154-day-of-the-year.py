70a493805c1a11a31cf5e81d83978df6d4c88759
Yuvraj Singh Chauhan <neelamsinghchauhan778@gmail.com>
2026-09-22 13:11:55 +0530

    Time: 15 ms (31.49%), Space: 19.3 MB (63.7%) - LeetHub

--- 1154-day-of-the-year/1154-day-of-the-year.py
+++ 1154-day-of-the-year/1154-day-of-the-year.py
@@ -1,34 +1,21 @@
 class Solution:
     def dayOfYear(self, date: str) -> int:
-        year=date[:4]
-        year=int(year)
-        months = {
-              1: 31,
-              2: 28,
-              3: 31,
-              4: 30,
-              5: 31,
-              6: 30,
-              7: 31,
-              8: 31,
-              9: 30,
-              10: 31,
-              11: 30,
-              12: 31
-                      }
+        year = int(date[:4])
 
-        if (year%4==0 and year%100!=0) or ( year %400==0):
-            months[2]=29
+        months = [31, 28, 31, 30, 31, 30,
+                  31, 31, 30, 31, 30, 31]
 
-        m=date[5:7]
-        m=int(m)
+        if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+            months[1] = 29
 
-        days=0
-        
-        for i in range(1,m):
-            days+=months[i]
-        
-        days+=int(date[8:10])
+        m = int(date[5:7])
+        d = int(date[8:10])
+
+        days = 0
+
+        for i in range(m - 1):
+            days += months[i]
+
+        days += d
 
         return days
-
